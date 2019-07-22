9757637c41e11104803f4aa54b98de85c9fe436d
Justinas Reigis <justinas.reigis@gmail.com>
2019-07-22 23:39:25 +0100

    duplicated elements improved (n) to log(n)

--- src/duplicatedElements.ts
+++ src/duplicatedElements.ts
@@ -1,32 +1,46 @@
 // Given a sorted array, A, with possibly duplicated elements, find the indices of the first and last occurrences of a target element, x. Return -1 if the target is not found.
 
-function solution(nums: number[], target: number): number[] {
+export function solution(nums: number[], target: number): number[] {
   const answer = [-1, -1];
   const len = nums.length;
-  if (len < 2) return answer;
+  if (len === 0) return answer;
   if (nums[0] > target) return answer;
   if (nums[len - 1] < target) return answer;
 
-  // lastindexof
-  // firstindexof
-
-  let hasDuplicate = false;
-  for (let i = 0; i < len; i++) {
-    const current = nums[i];
-    if (current < target) continue;
-    if (current > target) break;
-    if (current === target) {
-      if (hasDuplicate) {
-        answer[1] = i;
-      } else {
-        answer[0] = i;
-        answer[1] = i;
-        hasDuplicate = true;
-      }
-    }
-  }
+  answer[0] = nums.indexOf(target);
+  answer[1] = nums.lastIndexOf(target);
 
   return answer;
 }
 
-export default solution;
+export function solutionCustom(nums: number[], target: number): number[] {
+  const answer = [-1, -1];
+  const len = nums.length;
+  if (len === 0) return answer;
+  if (nums[0] > target) return answer;
+  if (nums[len - 1] < target) return answer;
+
+  const leftMost = extremeBinarySearch(nums, target, true);
+  // ensure number exists
+  if (nums[leftMost] !== target) {
+    return answer;
+  }
+  answer[0] = leftMost;
+  answer[1] = extremeBinarySearch(nums, target, false) - 1;
+
+  return answer;
+}
+
+function extremeBinarySearch(nums: number[], targer: number, left: boolean): number {
+  let lo = 0;
+  let hi = nums.length;
+  while (lo < hi) {
+    const mid = Math.trunc(lo + (hi - lo) / 2); // not to overflow
+    if (nums[mid] > targer || (left && nums[mid] === targer)) {
+      hi = mid; // start to mid
+    } else {
+      lo = mid + 1; // mid + 1 to end
+    }
+  }
+  return lo;
+}
--- test/duplicatedElements.test.ts
+++ test/duplicatedElements.test.ts
@@ -1,37 +1,50 @@
-import solution from '../src/duplicatedElements';
+import { solution, solutionCustom } from '../src/duplicatedElements';
 
 test('test case 1', () => {
   const input = [1, 3, 3, 5, 7, 8, 9, 9, 9, 15];
   const target = 9;
   expect(solution(input, target)).toStrictEqual([6, 8]);
+  expect(solutionCustom(input, target)).toStrictEqual([6, 8]);
 });
 
 test('test case 2', () => {
   const input = [100, 150, 150, 153];
   const target = 150;
   expect(solution(input, target)).toStrictEqual([1, 2]);
+  expect(solutionCustom(input, target)).toStrictEqual([1, 2]);
 });
 
 test('test case 3', () => {
   const input = [1, 2, 3, 4, 5, 6, 10];
   const target = 9;
   expect(solution(input, target)).toStrictEqual([-1, -1]);
+  expect(solutionCustom(input, target)).toStrictEqual([-1, -1]);
 });
 
 test('test case 4', () => {
   const input = [1, 2, 3, 4, 5, 6, 10];
   const target = 2;
   expect(solution(input, target)).toStrictEqual([1, 1]);
+  expect(solutionCustom(input, target)).toStrictEqual([1, 1]);
 });
 
 test('test case 5', () => {
   const input = [5, 7, 7, 8, 8, 10];
   const target = 8;
   expect(solution(input, target)).toStrictEqual([3, 4]);
+  expect(solutionCustom(input, target)).toStrictEqual([3, 4]);
 });
 
 test('test case 6', () => {
   const input = [5, 7, 7, 8, 8, 10];
   const target = 6;
   expect(solution(input, target)).toStrictEqual([-1, -1]);
+  expect(solutionCustom(input, target)).toStrictEqual([-1, -1]);
+});
+
+test('test case 7', () => {
+  const input = [1];
+  const target = 1;
+  expect(solution(input, target)).toStrictEqual([0, 0]);
+  expect(solutionCustom(input, target)).toStrictEqual([0, 0]);
 });
