36ec710eb3dc6e2c266379cb55b37917200c926d
Justinas Reigis <justinas.reigis@gmail.com>
2019-08-17 14:24:13 +0100

    optimization for 8.4 - sanitise input only once

--- src/RecursionAndDynamicProgramming/PowerSet/powerSet.ts
+++ src/RecursionAndDynamicProgramming/PowerSet/powerSet.ts
@@ -1,15 +1,23 @@
-export const solution = (set: string[]): string[][] => {
-  const sanitiseInput = [...new Set(set)];
-  if (sanitiseInput.length === 0) return [sanitiseInput];
+const powerSet = (set: string[]): string[][] => {
+  if (set.length === 0) return [set];
 
-  const e: string = sanitiseInput.pop() || '';
+  // the following block is to silence eslint
+  // as .pop() may return undefined
+  // but the length is checked at the top of the function
+  const el = set.pop();
+  if (!el) throw new Error('.pop() returned undefined');
+  const e: string = el;
 
-  const pt = solution(sanitiseInput);
-
+  const pt = powerSet(set);
   const fept: string[][] = [];
   pt.forEach(x => {
     fept.push(x.concat([e]));
   });
 
   return fept.concat(pt);
+};
+
+export const solution = (set: string[]): string[][] => {
+  const sanitiseInput = [...new Set(set)];
+  return powerSet(sanitiseInput);
 };
