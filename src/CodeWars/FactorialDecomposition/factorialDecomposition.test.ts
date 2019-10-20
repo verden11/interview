93e3ee400be023f3fd0439d0bffa6c852937f526
Justinas Reigis <justinas.reigis@gmail.com>
2019-10-20 12:09:04 +0100

    codewars
    Factorial decomposition

---
+++ src/CodeWars/FactorialDecomposition/factorialDecomposition.test.ts
@@ -0,0 +1,14 @@
+import { solution } from './factorialDecomposition';
+
+describe('Factorial Decomposition', () => {
+  it('basic cases', () => {
+    expect(solution(2)).toStrictEqual('2');
+    expect(solution(3)).toStrictEqual('3');
+    expect(solution(7)).toStrictEqual('7');
+  });
+
+  it('n < 20 cases', () => {
+    expect(solution(5)).toStrictEqual('2^3 * 3 * 5');
+    expect(solution(14)).toStrictEqual('2^11 * 3^5 * 5^2 * 7^2 * 11 * 13');
+  });
+});
---
+++ src/CodeWars/FactorialDecomposition/factorialDecomposition.ts
@@ -0,0 +1,85 @@
+const primes: number[] = [2, 3];
+
+const isPrime = (n: number): boolean => {
+  if (n <= 1) return false;
+  if (n < 4) return true; // 2 and 3 are primes
+  if (n % 2 === 0) return false;
+  if (n < 9) return true; // 4, 6, and 8 already excluded
+  if (n % 3 === 0) return false;
+  const r = Math.floor(Math.sqrt(n));
+  let f = 5;
+  while (f <= r) {
+    if (n % f === 0) return false;
+    if (n % (f + 2) === 0) return false;
+    f += 6;
+  }
+  return true;
+};
+
+const findNextPrime = (n: number): number => {
+  while (true) {
+    n += 2;
+    if (isPrime(n)) {
+      primes.push(n);
+      return n;
+    }
+  }
+};
+
+const factorNumber = (n: number): Map<number, number> => {
+  const map = new Map<number, number>();
+  let counter = 0;
+  while (n !== 1) {
+    const prime = primes[counter] || findNextPrime(primes[counter - 1]);
+    const tmp = Math.floor(n / prime);
+    if (tmp * prime === n) {
+      n /= prime;
+      map.set(prime, (map.get(prime) || 0) + 1);
+    } else {
+      counter++;
+    }
+  }
+
+  return map;
+};
+
+export function decomp(n: number): string {
+  const map = new Map();
+  for (let i = 2; i <= n; i++) {
+    const tmpMap = factorNumber(i);
+    for (const entry of tmpMap.keys()) {
+      map.set(entry, (map.get(entry) || 0) + tmpMap.get(entry));
+    }
+  }
+
+  let ret = '';
+  // build return string
+  for (const entry of map.entries()) {
+    const key = entry[0];
+    const value = entry[1];
+    ret += value > 1 ? `${key}^${value}` : key;
+    ret += ' * ';
+  }
+
+  return ret.slice(0, -3);
+}
+
+export function decomp2(n: number): string {
+  const map = new Map();
+  const tmpMap = factorNumber(n);
+  for (const entry of tmpMap.keys()) {
+    map.set(entry, (map.get(entry) || 0) + tmpMap.get(entry));
+  }
+
+  let ret = '';
+  // build return string
+  for (const entry of map.entries()) {
+    const key = entry[0];
+    const value = entry[1];
+    ret += value > 1 ? `(${key}**${value})` : `(${key})`;
+  }
+
+  return ret;
+}
+
+console.log(decomp2(7775460));
