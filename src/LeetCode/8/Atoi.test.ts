a7b083cf69e98265ffa2115ae0142b9c43cde097
verden11 <justinas.reigis@gmail.com>
2019-11-24 21:57:04 +0200

    leetcode string to integer atoi

---
+++ src/LeetCode/8/Atoi.test.ts
@@ -0,0 +1,31 @@
+import { solution } from './Atoi';
+
+test('simple', (): void => {
+  const input = '42';
+  const restult = solution(input);
+  expect(restult).toBe(42);
+});
+
+test('with whitespace', (): void => {
+  const input = '  -42';
+  const result = solution(input);
+  expect(result).toBe(-42);
+});
+
+test('with words', (): void => {
+  const input = '4193 with words';
+  const restult = solution(input);
+  expect(restult).toBe(4193);
+});
+
+test('words and number', (): void => {
+  const input = 'text and 987';
+  const result = solution(input);
+  expect(result).toBe(0);
+});
+
+test('large number', (): void => {
+  const input = '-91283472332';
+  const result = solution(input);
+  expect(result).toBe(Math.pow(-2, 31));
+});
---
+++ src/LeetCode/8/Atoi.ts
@@ -0,0 +1,9 @@
+export const solution = (s: string): number => {
+  const result = Number.parseInt(s, 10);
+  if (isNaN(result)) return 0;
+
+  // check if the number is 32-bit signed integer [-2^31, 2^31 -1]
+  const power = Math.log2(Math.abs(result));
+  if (power > 30) return result > 0 ? 2 ** 31 - 1 : (-2) ** 31;
+  return result;
+};
